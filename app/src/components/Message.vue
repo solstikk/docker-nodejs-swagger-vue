4109661f7b12ade8ae198ed5a795a47337a07f8a
Gunnar Braaten <gunnar.braaten@gmail.com>
2017-10-03 22:10:50 +0200

    Adding a vue component.

---
+++ app/src/components/Message.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .box { background: #e3e3e3; padding: 20px; border: 1px; }
+</style>
